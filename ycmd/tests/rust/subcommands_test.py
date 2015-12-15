421a9d850c136aaa2bb1e7b11f3c3fc321ba1e03
Joe Wilm <joe@jwilm.com>
2015-12-15 14:16:46 -0800

    Add rust completer/goto tests
    RustCompleter._StopServer is now idempotent. This is critical for tests
    because, in the case of test failure, racerd should still be stopped.

---
+++ ycmd/tests/rust/subcommands_test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2015 ycmd contributors.
+#
+# This file is part of ycmd.
+#
+# ycmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ycmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
+
+# from webtest import AppError
+# from nose.tools import eq_
+# from hamcrest import assert_that, raises, calling
+
+from rust_handlers_test import Rust_Handlers_test
+from nose.tools import eq_
+
+class Rust_Subcommands_test( Rust_Handlers_test ):
+  def GetType_Basic_test( self ):
+    filepath = self._PathToTestFile( 'test.rs' )
+    contents = open( filepath ).read()
+
+    event_data = self._BuildRequest( filepath = filepath,
+                                     filetype = 'rust',
+                                     contents = contents,
+                                     event_name = 'BufferVisit' )
+
+    self._app.post_json( '/event_notification', event_data )
+
+    goto_data = self._BuildRequest( completer_target = 'filetype_default',
+                                    command_arguments = [ 'GoTo' ],
+                                    line_num = 7,
+                                    column_num = 12,
+                                    contents = contents,
+                                    filetype = 'rust',
+                                    filepath = filepath )
+
+    results = self._app.post_json( '/run_completer_command',
+                                   goto_data )
+
+    eq_( {
+      'line_num': 1, 'column_num': 8, 'filepath': filepath
+    }, results.json )
---
+++ ycmd/tests/rust/testdata/test.rs
@@ -0,0 +1,10 @@
+pub fn create_universe() { }
+pub fn build_rocket() { }
+pub fn build_shuttle() { }
+
+pub fn main() {
+    // Identified here can be used for GoTo test
+    create_universe();
+    // Test completion
+    build_
+}
