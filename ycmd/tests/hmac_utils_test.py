f9ec287bdf237787117b369c55b7bae9e3511e5e
micbou <contact@micbou.com>
2016-02-20 23:44:46 +0100

    Use skip nosetests plugin for conditional tests

--- ycmd/tests/hmac_utils_test.py
+++ ycmd/tests/hmac_utils_test.py
@@ -26,6 +26,7 @@ from future.utils import PY2
 
 from binascii import hexlify
 from nose.tools import eq_, ok_, raises
+from nose.plugins.skip import SkipTest
 from ycmd import hmac_utils as hu
 
 
@@ -38,14 +39,16 @@ def CreateHmac_WithBytes_test():
            b'ef4d59a14946175997479dbc2d1a3cd8' ) )
 
 
-if PY2:
-  def CreateHmac_WithPy2Str_test():
-    # Test vectors from Wikipedia (HMAC_SHA256): https://goo.gl/cvX0Tn
-    eq_( hexlify( hu.CreateHmac(
-      'The quick brown fox jumps over the lazy dog',
-      'key' ) ),
-      'f7bc83f430538424b13298e6aa6fb143'
-      'ef4d59a14946175997479dbc2d1a3cd8' )
+def CreateHmac_WithPy2Str_test():
+  if not PY2:
+    raise SkipTest
+
+  # Test vectors from Wikipedia (HMAC_SHA256): https://goo.gl/cvX0Tn
+  eq_( hexlify( hu.CreateHmac(
+    'The quick brown fox jumps over the lazy dog',
+    'key' ) ),
+    'f7bc83f430538424b13298e6aa6fb143'
+    'ef4d59a14946175997479dbc2d1a3cd8' )
 
 
 def CreateRequestHmac_WithBytes_test():
@@ -58,15 +61,17 @@ def CreateRequestHmac_WithBytes_test():
            b'e58bb8974166eaf20e0224d999894b34' ) )
 
 
-if PY2:
-  def CreateRequestHmac_WithPy2Str_test():
-    eq_( hexlify( hu.CreateRequestHmac(
-      'GET',
-      '/foo',
-      'body',
-      'key' ) ),
-      'bfbb6bc7a2b3eca2a78f4e7ec8a7dfa7'
-      'e58bb8974166eaf20e0224d999894b34' )
+def CreateRequestHmac_WithPy2Str_test():
+  if not PY2:
+    raise SkipTest
+
+  eq_( hexlify( hu.CreateRequestHmac(
+    'GET',
+    '/foo',
+    'body',
+    'key' ) ),
+    'bfbb6bc7a2b3eca2a78f4e7ec8a7dfa7'
+    'e58bb8974166eaf20e0224d999894b34' )
 
 
 def SecureBytesEqual_Basic_test():
@@ -83,7 +88,9 @@ def SecureBytesEqual_ExceptionOnUnicode_test():
   ok_( hu.SecureBytesEqual( u'foo', u'foo' ) )
 
 
-if PY2:
-  @raises( TypeError )
-  def SecureBytesEqual_ExceptionOnPy2Str_test():
-    ok_( hu.SecureBytesEqual( 'foo', 'foo' ) )
+@raises( TypeError )
+def SecureBytesEqual_ExceptionOnPy2Str_test():
+  if not PY2:
+    raise SkipTest
+
+  ok_( hu.SecureBytesEqual( 'foo', 'foo' ) )
--- ycmd/tests/utils_test.py
+++ ycmd/tests/utils_test.py
@@ -26,6 +26,7 @@ from builtins import *  # noqa
 from future.utils import PY2, native
 from mock import patch, call
 from nose.tools import eq_, ok_
+from nose.plugins.skip import SkipTest
 from ycmd import utils
 from ycmd.tests.test_utils import PathToTestFile
 from shutil import rmtree
@@ -36,35 +37,49 @@ import subprocess
 # changing things here, read the comments in utils.ToBytes.
 
 
-if PY2:
-  def ToBytes_Py2Bytes_test():
-    value = utils.ToBytes( bytes( 'abc' ) )
-    eq_( value, bytes( 'abc' ) )
-    eq_( type( value ), bytes )
+def ToBytes_Py2Bytes_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToBytes( bytes( 'abc' ) )
+  eq_( value, bytes( 'abc' ) )
+  eq_( type( value ), bytes )
 
 
-  def ToBytes_Py2Str_test():
-    value = utils.ToBytes( 'abc' )
-    eq_( value, bytes( 'abc' ) )
-    eq_( type( value ), bytes )
+def ToBytes_Py2Str_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToBytes( 'abc' )
+  eq_( value, bytes( 'abc' ) )
+  eq_( type( value ), bytes )
 
 
-  def ToBytes_Py2FutureStr_test():
-    value = utils.ToBytes( str( 'abc' ) )
-    eq_( value, bytes( 'abc' ) )
-    eq_( type( value ), bytes )
+def ToBytes_Py2FutureStr_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToBytes( str( 'abc' ) )
+  eq_( value, bytes( 'abc' ) )
+  eq_( type( value ), bytes )
 
 
-  def ToBytes_Py2Unicode_test():
-    value = utils.ToBytes( u'abc' )
-    eq_( value, bytes( 'abc' ) )
-    eq_( type( value ), bytes )
+def ToBytes_Py2Unicode_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToBytes( u'abc' )
+  eq_( value, bytes( 'abc' ) )
+  eq_( type( value ), bytes )
 
 
-  def ToBytes_Py2Int_test():
-    value = utils.ToBytes( 123 )
-    eq_( value, bytes( '123' ) )
-    eq_( type( value ), bytes )
+def ToBytes_Py2Int_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToBytes( 123 )
+  eq_( value, bytes( '123' ) )
+  eq_( type( value ), bytes )
 
 
 def ToBytes_Bytes_test():
@@ -85,35 +100,49 @@ def ToBytes_Int_test():
   eq_( type( value ), bytes )
 
 
-if PY2:
-  def ToUnicode_Py2Bytes_test():
-    value = utils.ToUnicode( bytes( 'abc' ) )
-    eq_( value, u'abc' )
-    ok_( isinstance( value, str ) )
+def ToUnicode_Py2Bytes_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToUnicode( bytes( 'abc' ) )
+  eq_( value, u'abc' )
+  ok_( isinstance( value, str ) )
 
 
-  def ToUnicode_Py2Str_test():
-    value = utils.ToUnicode( 'abc' )
-    eq_( value, u'abc' )
-    ok_( isinstance( value, str ) )
+def ToUnicode_Py2Str_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToUnicode( 'abc' )
+  eq_( value, u'abc' )
+  ok_( isinstance( value, str ) )
 
 
-  def ToUnicode_Py2FutureStr_test():
-    value = utils.ToUnicode( str( 'abc' ) )
-    eq_( value, u'abc' )
-    ok_( isinstance( value, str ) )
+def ToUnicode_Py2FutureStr_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToUnicode( str( 'abc' ) )
+  eq_( value, u'abc' )
+  ok_( isinstance( value, str ) )
 
 
-  def ToUnicode_Py2Unicode_test():
-    value = utils.ToUnicode( u'abc' )
-    eq_( value, u'abc' )
-    ok_( isinstance( value, str ) )
+def ToUnicode_Py2Unicode_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToUnicode( u'abc' )
+  eq_( value, u'abc' )
+  ok_( isinstance( value, str ) )
 
 
-  def ToUnicode_Py2Int_test():
-    value = utils.ToUnicode( 123 )
-    eq_( value, u'123' )
-    ok_( isinstance( value, str ) )
+def ToUnicode_Py2Int_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToUnicode( 123 )
+  eq_( value, u'123' )
+  ok_( isinstance( value, str ) )
 
 
 def ToUnicode_Bytes_test():
@@ -134,23 +163,31 @@ def ToUnicode_Int_test():
   ok_( isinstance( value, str ) )
 
 
-if PY2:
-  def ToCppStringCompatible_Py2Str_test():
-    value = utils.ToCppStringCompatible( 'abc' )
-    eq_( value, 'abc' )
-    eq_( type( value ), type( '' ) )
+def ToCppStringCompatible_Py2Str_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToCppStringCompatible( 'abc' )
+  eq_( value, 'abc' )
+  eq_( type( value ), type( '' ) )
 
 
-  def ToCppStringCompatible_Py2Unicode_test():
-    value = utils.ToCppStringCompatible( u'abc' )
-    eq_( value, 'abc' )
-    eq_( type( value ), type( '' ) )
+def ToCppStringCompatible_Py2Unicode_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToCppStringCompatible( u'abc' )
+  eq_( value, 'abc' )
+  eq_( type( value ), type( '' ) )
 
 
-  def ToCppStringCompatible_Py2Int_test():
-    value = utils.ToCppStringCompatible( 123 )
-    eq_( value, '123' )
-    eq_( type( value ), type( '' ) )
+def ToCppStringCompatible_Py2Int_test():
+  if not PY2:
+    raise SkipTest
+
+  value = utils.ToCppStringCompatible( 123 )
+  eq_( value, '123' )
+  eq_( type( value ), type( '' ) )
 
 
 def ToCppStringCompatible_Bytes_test():
@@ -270,12 +307,14 @@ def SetEnviron_UnicodeNotOnWindows_test( *args ):
   eq_( env, { u'key': u'value' } )
 
 
-if PY2:
-  @patch( 'ycmd.utils.OnWindows', return_value = True )
-  def SetEnviron_UnicodeOnWindows_test( *args ):
-    env = {}
-    utils.SetEnviron( env, u'key', u'value' )
-    eq_( env, { native( bytes( b'key' ) ): native( bytes( b'value' ) ) } )
+@patch( 'ycmd.utils.OnWindows', return_value = True )
+def SetEnviron_UnicodeOnWindows_test( *args ):
+  if not PY2:
+    raise SkipTest
+
+  env = {}
+  utils.SetEnviron( env, u'key', u'value' )
+  eq_( env, { native( bytes( b'key' ) ): native( bytes( b'value' ) ) } )
 
 
 def PathsToAllParentFolders_Basic_test():
@@ -302,11 +341,13 @@ def PathsToAllParentFolders_FileAtRoot_test():
        list( utils.PathsToAllParentFolders( '/test.c' ) ) )
 
 
-if utils.OnWindows():
-  def PathsToAllParentFolders_WindowsPath_test():
-    eq_( [
-      os.path.normpath( r'C:\\foo\\goo\\zoo' ),
-      os.path.normpath( r'C:\\foo\\goo' ),
-      os.path.normpath( r'C:\\foo' ),
-      os.path.normpath( r'C:\\' )
-    ], list( utils.PathsToAllParentFolders( r'C:\\foo\\goo\\zoo\\test.c' ) ) )
+def PathsToAllParentFolders_WindowsPath_test():
+  if not utils.OnWindows():
+    raise SkipTest
+
+  eq_( [
+    os.path.normpath( r'C:\\foo\\goo\\zoo' ),
+    os.path.normpath( r'C:\\foo\\goo' ),
+    os.path.normpath( r'C:\\foo' ),
+    os.path.normpath( r'C:\\' )
+  ], list( utils.PathsToAllParentFolders( r'C:\\foo\\goo\\zoo\\test.c' ) ) )
