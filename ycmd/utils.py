95af122ffc7a82fe65bd4c9ce70643efd74d1b14
Andrea Cedraro <a.cedraro@gmail.com>
2016-03-12 16:52:19 +0100

    Search user defined python binary also in PATH

--- ycmd/utils.py
+++ ycmd/utils.py
@@ -36,6 +36,7 @@ import subprocess
 # Creation flag to disable creating a console window on Windows. See
 # https://msdn.microsoft.com/en-us/library/windows/desktop/ms684863.aspx
 CREATE_NO_WINDOW = 0x08000000
+# XXX(vheon): should we take these from the PATHEXT env variable?
 # Executable extensions used on Windows
 WIN_EXECUTABLE_EXTS = [ '.exe', '.bat', '.cmd' ]
 
@@ -46,6 +47,8 @@ RAW_PATH_TO_TEMP_DIR = os.path.join( tempfile.gettempdir(), 'ycm_temp' )
 # Readable, writable and executable by everyone.
 ACCESSIBLE_TO_ALL_MASK = ( stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH |
                            stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP )
+
+EXECUTABLE_FILE_MASK = os.F_OK | os.X_OK
 
 
 # Python 3 complains on the common open(path).read() idiom because the file
@@ -165,33 +168,59 @@ def RemoveIfExists( filename ):
 
 def PathToFirstExistingExecutable( executable_name_list ):
   for executable_name in executable_name_list:
-    path = FindExecutable( executable_name )
+    path = Which( executable_name )
     if path:
       return path
   return None
 
 
+# Check that a given file can be accessed as an executable file.
+# Additionally check that `file` is not a directory, as on Windows
+# directories pass the os.access check.
+def IsExecutable( filename ):
+  return ( os.path.exists( filename )
+            and os.access( filename, EXECUTABLE_FILE_MASK )
+            and not os.path.isdir( filename ) )
+
+
 # On Windows, distutils.spawn.find_executable only works for .exe files
 # but .bat and .cmd files are also executables, so we use our own
 # implementation.
-def FindExecutable( executable ):
+# Adapted from https://hg.python.org/cpython/file/3.5/Lib/shutil.py#l1081
+def Which( executable ):
+
+  # If we're given a path with a directory part, look it up directly rather
+  # than referring to PATH directories. This includes checking relative to the
+  # current directory, e.g. ./script
+  if os.path.dirname( executable ):
+    if IsExecutable( executable ):
+      return executable
+    return None
+
   paths = os.environ[ 'PATH' ].split( os.pathsep )
   base, extension = os.path.splitext( executable )
 
-  if OnWindows() and extension.lower() not in WIN_EXECUTABLE_EXTS:
-    extensions = WIN_EXECUTABLE_EXTS
-  else:
-    extensions = ['']
+  if OnWindows():
+    # The current directory takes precedence on Windows.
+    if os.curdir not in paths:
+      paths.insert( 0, os.curdir )
 
-  for extension in extensions:
-    executable_name = executable + extension
-    if not os.path.isfile( executable_name ):
-      for path in paths:
-        executable_path = os.path.join(path, executable_name )
-        if os.path.isfile( executable_path ):
-          return executable_path
+    # See if the given file matches any of the expected path extensions.
+    # This will allow us to short circuit when given "python.exe".
+    # If it does match, only test that one, otherwise we have to try
+    # others.
+    if extension in WIN_EXECUTABLE_EXTS:
+      files = [ executable ]
     else:
-      return executable_name
+      files = [ base + ext for ext in WIN_EXECUTABLE_EXTS ]
+  else:
+    files = [ executable ]
+
+  for path in paths:
+    for file in files:
+      name = os.path.join( path, file )
+      if IsExecutable( name ):
+        return name
   return None
 
 
