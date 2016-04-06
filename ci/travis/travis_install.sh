eb5c9e1f14f750ddfe09473d1056c08a1998f9d3
micbou <contact@micbou.com>
2016-04-06 15:58:52 +0200

    Fix coverage build on Travis

--- ci/travis/travis_install.sh
+++ ci/travis/travis_install.sh
@@ -60,7 +60,7 @@ npm install -g typescript
 # Enable coverage for Python subprocesses. See:
 # http://coverage.readthedocs.org/en/coverage-4.0.3/subprocess.html
 echo -e "import coverage\ncoverage.process_startup()" > \
-  ${YCMD_VENV_DIR}/lib/python${YCMD_PYTHON_VERSION}/sitecustomize.py
+  ${PYENV_ROOT}/versions/${PYENV_VERSION}/lib/python${YCMD_PYTHON_VERSION}/site-packages/sitecustomize.py
 
 ############
 # rust setup
