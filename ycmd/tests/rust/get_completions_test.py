f3f4a9c3a337638ff93248c19a64352fb4754fdb
Val Markovic <val@markovic.io>
2016-06-18 13:26:12 -0700

    Also checking that rust_src_path exists
    We were only checking that the path was set to something, not that there
    was something there. This blows up in the user's face if they have
    a common editor config across machines that gets synced but they forget
    to actually place the required files in that location.

--- ycmd/tests/rust/get_completions_test.py
+++ ycmd/tests/rust/get_completions_test.py
@@ -25,6 +25,7 @@ from builtins import *  # noqa
 
 from hamcrest import assert_that, empty, has_entry, has_items, contains_string
 from nose.tools import eq_
+from mock import patch
 
 from ycmd.tests.rust import IsolatedYcmd, PathToTestFile, SharedYcmd
 from ycmd.tests.test_utils import BuildRequest, CompletionEntryMatcher
@@ -57,6 +58,29 @@ def GetCompletions_Basic_test( app ):
 @IsolatedYcmd
 def GetCompletions_WhenStandardLibraryCompletionFails_MentionRustSrcPath_test(
   app ):
+  filepath = PathToTestFile( 'std_completions.rs' )
+  contents = ReadFile( filepath )
+
+  completion_data = BuildRequest( filepath = filepath,
+                                  filetype = 'rust',
+                                  contents = contents,
+                                  force_semantic = True,
+                                  line_num = 5,
+                                  column_num = 11 )
+
+  response = app.post_json( '/completions',
+                            completion_data,
+                            expect_errors = True ).json
+  assert_that( response,
+               has_entry( 'message',
+                          contains_string( 'rust_src_path' ) ) )
+
+
+# This test is isolated because it affects the GoTo tests, although it
+# shouldn't.
+@IsolatedYcmd
+@patch.dict( 'os.environ', { 'RUST_SRC_PATH': '/foobar' } )
+def GetCompletions_RustSrcPathSetButDoesntExist_test( app ):
   filepath = PathToTestFile( 'std_completions.rs' )
   contents = ReadFile( filepath )
 
