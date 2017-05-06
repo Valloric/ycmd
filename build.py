0aad2839b254763768a0f8ef0329bf7ccf051bea
Jerry Marino <i@jerrymarino.com>
2017-05-06 15:58:15 -0700

    Swift Semantic Completion
    This patch adds support for Swift semantic completion.
    
    It adds a new subclass of `Completer` which is a thin client for
    SwiftySwiftVim, the semantic backend.
    
    SwiftySwiftVim is an HTTP interface to semantic facilities in the Swift
    Compiler, tailored to the needs of a text editor.
    
    Usage:
    
    After you've met all of the system dependencies, then simply run
    build.py with --swift-completer. This will checkout and build
    SwiftySwiftVim from source in `third_party/swiftyswiftvim`.
    
    Build Summary:
    
    To build SwiftySwiftVim from source, you need a few `system` dependencies:
    - OSX ( for Xcode )
    - Xcode ( for prebuilt swift binary )
    - Boost
    
    See https://github.com/jerrymarino/swiftyswiftvim for detailed information.

--- build.py
+++ build.py
@@ -241,6 +241,8 @@ def ParseArguments():
                        'from llvm.org. NOT RECOMMENDED OR SUPPORTED!' )
   parser.add_argument( '--omnisharp-completer', action = 'store_true',
                        help = 'Build C# semantic completion engine.' )
+  parser.add_argument( '--swift-completer', action = 'store_true',
+                       help = 'Build swift semantic completion engine.' )
   parser.add_argument( '--gocode-completer', action = 'store_true',
                        help = 'Build Go semantic completion engine.' )
   parser.add_argument( '--racer-completer', action = 'store_true',
@@ -424,6 +426,9 @@ def BuildGoCode():
   os.chdir( p.join( DIR_OF_THIS_SCRIPT, 'third_party', 'godef' ) )
   CheckCall( [ 'go', 'build' ] )
 
+def BuildSwiftySwiftVim():
+  os.chdir( p.join( DIR_OF_THIS_SCRIPT, 'third_party', 'swiftyswiftvim' ) )
+  CheckCall( [ 'bash', 'bootstrap' ] )
 
 def BuildRacerd():
   """
@@ -491,6 +496,8 @@ def Main():
     SetUpTern()
   if args.racer_completer or args.all_completers:
     BuildRacerd()
+  if args.swift_completer or args.all_completers:
+    BuildSwiftySwiftVim()
   WritePythonUsedDuringBuild()
 
 
