2b4900a377abacb883de14acc44776351477ab90
Ben Jackson <puremourning@gmail.com>
2017-11-08 20:01:56 +0000

    Use SharedYcmd for handler tests

--- ycmd/tests/misc_handlers_test.py
+++ ycmd/tests/misc_handlers_test.py
@@ -199,14 +199,14 @@ def MiscHandlers_DebugInfo_ExtraConfFoundButNotLoaded_test( app ):
   )
 
 
-@IsolatedYcmd()
+@SharedYcmd
 def MiscHandlers_ReceiveMessages_NoCompleter_test( app ):
   request_data = BuildRequest()
   assert_that( app.post_json( '/receive_messages', request_data ).json,
                equal_to( False ) )
 
 
-@IsolatedYcmd()
+@SharedYcmd
 def MiscHandlers_ReceiveMessages_NotSupportedByCompleter_test( app ):
   request_data = BuildRequest( filetype = 'python' )
   assert_that( app.post_json( '/receive_messages', request_data ).json,
