af48b95c9d8e3d4cdb726c87ea0e3db196e2bff2
micbou <contact@micbou.com>
2018-12-03 12:29:25 +0100

    Fix system headers search on macOS
    We can't rely on upstream libclang to find the system headers on macOS
    because it ignores the -resource-dir flag and use the current working
    directory to find the builtin includes instead, which fails since they
    are in a different place (in the clang_includes folder). Furthermore,
    it's unable to find the framework headers on macOS 10.14 without setting
    the sysroot. So, we replicate the directory structure used by upstream
    Clang to allow the libclang library to find the builtin includes and we
    try to reproduce the logic used by Apple Clang to find the system
    headers by looking at the output of the command
    
      clang++ -x c++ -E -v -
    
    which prints the list of system header directories and by reading the
    source code of upstream Clang.

--- ycmd/__main__.py
+++ ycmd/__main__.py
@@ -42,7 +42,7 @@ import base64
 
 from ycmd import extra_conf_store, user_options_store, utils
 from ycmd.hmac_plugin import HmacPlugin
-from ycmd.utils import ( CompatibleWithCurrentCore,
+from ycmd.utils import ( ImportAndCheckCore,
                          OpenForStdHandle,
                          ReadFile,
                          ToBytes )
@@ -161,9 +161,10 @@ def Main():
   # This ensures that ycm_core is not loaded before extra conf
   # preload was run.
   YcmCoreSanityCheck()
+
   extra_conf_store.CallGlobalExtraConfYcmCorePreloadIfExists()
 
-  code = CompatibleWithCurrentCore()
+  code = ImportAndCheckCore()
   if code:
     sys.exit( code )
 
