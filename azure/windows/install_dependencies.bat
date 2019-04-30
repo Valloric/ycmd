819339ec21850490aaea237dd8b7350e53e752ca
micbou <contact@micbou.com>
2019-04-30 13:56:09 +0200

    Set up CI with Azure Pipelines

---
+++ azure/windows/install_dependencies.bat
@@ -0,0 +1,34 @@
+set python_installer_extension=%YCM_PYTHON_INSTALLER_URL:~-3%
+
+::
+:: Python configuration
+::
+
+if "%python_installer_extension%" == "exe" (
+  curl %YCM_PYTHON_INSTALLER_URL% -o C:\python-installer.exe
+  ::C:\python-installer.exe /quiet PrependPath=1 InstallAllUsers=1 Include_launcher=1 InstallLauncherAllUsers=1 Include_test=0 Include_doc=0 Include_dev=1 Include_debug=0 Include_tcltk=0 TargetDir=C:\Python
+  C:\python-installer.exe /quiet TargetDir=C:\Python
+) else (
+  curl %YCM_PYTHON_INSTALLER_URL% -o C:\python-installer.msi
+  msiexec /i C:\python-installer.msi TARGETDIR=C:\Python /qn
+)
+
+C:\Python\Scripts\pip install -r test_requirements.txt
+
+:: Enable coverage for Python subprocesses. See:
+:: http://coverage.readthedocs.io/en/latest/subprocess.html
+C:\Python\python -c "with open('C:\Python\Lib\site-packages\sitecustomize.py', 'w') as f: f.write('import coverage\ncoverage.process_startup()')"
+
+::
+:: Rust configuration
+::
+
+curl https://win.rustup.rs/x86_64 -o rustup-init.exe
+rustup-init.exe -y
+
+::
+:: Go configuration
+::
+
+curl https://dl.google.com/go/go1.12.4.windows-amd64.msi -o C:\go-installer.msi
+msiexec /i C:\go-installer.msi TARGETDIR=C:\Go /qn
