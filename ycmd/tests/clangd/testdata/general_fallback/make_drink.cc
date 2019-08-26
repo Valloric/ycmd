ed22277e0cbfc3a8ff6a2600e4fb9a24ad196f01
Ben Jackson <puremourning@gmail.com>
2019-08-26 11:58:18 +0100

    Add some awkward test cases

--- ycmd/tests/clangd/testdata/general_fallback/make_drink.cc
+++ ycmd/tests/clangd/testdata/general_fallback/make_drink.cc
@@ -7,3 +7,17 @@ int main( int , char ** )
   make_drink( TypeOfDrink::COFFEE, 10.0, Flavour::ELDERFLOWER );
   make_drink( TypeOfDrink::JUICE, Temperature::COLD, 1 );
 }
+
+void test_right_edge_80()
+{
+                                                                     make_drink(
+                                                                  TypeOfDrink::COFFEE,
+                                                                  10, Flavour::ORANGE_AND_PINEAPPLE);
+}
+
+void test_left_edge()
+{
+make_drink( TypeOfDrink::JUICE,
+            Temperature
+::WARM, 10 );
+}
