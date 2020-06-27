cb400a0c2fafa2ab5945ff38f1557900b99a833b
Ben Jackson <puremourning@gmail.com>
2020-06-27 19:14:49 +0100

    Remove warning-to-error hacks

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,7 @@ import subprocess
 import os.path as p
 import sys
 
-BASE_PYTEST_ARGS = [ '-v', '--color=yes', '-W', 'error::ResourceWarning' ]
+BASE_PYTEST_ARGS = [ '-v', '--color=yes' ]
 
 DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
 DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
