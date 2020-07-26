bca48ba6addce728d9d0bd39e88ad3411a637b61
Boris Staletic <boris.staletic@gmail.com>
2020-07-26 11:26:45 +0200

    Update gopls to 0.43

--- ycmd/completers/go/go_completer.py
+++ ycmd/completers/go/go_completer.py
@@ -98,20 +98,13 @@ class GoCompleter( language_server_completer.LanguageServerCompleter ):
 
 
   def DefaultSettings( self, request_data ):
-    return {
-      'hoverKind': 'Structured',
-      'fuzzyMatching': False,
-    }
+    return { 'hoverKind': 'Structured' }
 
 
-  def CodeActionLiteralToFixIt( self, request_data, code_action_literal ):
-    document_changes = code_action_literal[ 'edit' ][ 'documentChanges' ]
-    for text_document_edit in document_changes:
-      for text_edit in text_document_edit[ 'edits' ]:
-        end_line = text_edit[ 'range' ][ 'end' ][ 'line' ]
-        # LSP offsets are zero based, plus `request_data[ 'lines' ]` contains
-        # a trailing empty line.
-        if end_line >= len( request_data[ 'lines' ] ) - 1:
-          return None
+  def ExtraCapabilities( self ):
+    return { 'workspace': { 'configuration': True } }
 
-    return super().CodeActionLiteralToFixIt( request_data, code_action_literal )
+
+  def WorkspaceConfigurationResponse( self, request ):
+    return [ self._settings.get( 'ls', {} )
+             for i in request[ 'params' ][ 'items' ] ]
