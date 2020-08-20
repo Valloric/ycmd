26904083cc9d5ebd129c25e26d7cffb020adb818
Boris Staletic <boris.staletic@gmail.com>
2020-08-20 14:13:58 +0200

    Fix java version check
    Azure sets the $JAVA_TOOL_OPTIONS variable by default, which breaks how
    we parse the java version. Are there other variables we should know
    about and reset?

--- build.py
+++ build.py
@@ -963,13 +963,19 @@ def EnableJavaScriptCompleter( args ):
 def CheckJavaVersion( required_version ):
   java = FindExecutableOrDie(
     'java',
-    f'java {required_version} is required to install JDT.LS' )
+    f'java { required_version } is required to install JDT.LS' )
   java_version = None
   try:
+    new_env = os.environ.copy()
+    try:
+      new_env.pop( 'JAVA_TOOL_OPTIONS' )
+    except KeyError:
+      print( new_env )
     java_version = int(
       subprocess.check_output(
         [ java, os.path.join( DIR_OF_THIS_SCRIPT, 'CheckJavaVersion.java' ) ],
-        stderr=subprocess.STDOUT )
+        stderr=subprocess.STDOUT,
+        env = new_env )
       .decode( 'utf-8' )
       .strip() )
   except subprocess.CalledProcessError:
